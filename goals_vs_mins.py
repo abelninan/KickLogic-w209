f1faf3f97737860ad087ac353c57bdb4f30af442
Abel Ninan <abel.j.ninan@gmail.com>
2023-10-30 14:12:45 -0400

    Update goals_vs_mins.py

--- goals_vs_mins.py
+++ goals_vs_mins.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-st.set_page_config(layout="wide", theme = "light")
+st.set_page_config(layout="wide")
 
 st.write("# Player-Role Analysis")
 
