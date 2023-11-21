dd3793fb0e7ed616bfcacd8783665fd888202d3b
Paul Cooper <paul.cooper@appiancorp.com>
2023-11-20 21:48:47 -0500

    Paul pushing club tab

--- Soccer_Streamlit.py
+++ Soccer_Streamlit.py
@@ -8,6 +8,9 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
+from geopy.geocoders import Nominatim
+from ctypes import alignment
+from vega_datasets import data
 
 # Function to load data
 @st.cache_data  # This function will be cached
@@ -315,6 +318,239 @@ def main2():
 
     combined_plot_2
 
+def get_lat_long(address):
+    geolocator = Nominatim(user_agent="My App")
+    try:
+        x = geolocator.geocode(address)
+        return x.latitude, x.longitude
+    except:
+        return np.nan, np.nan
+        
+def calc_action_weight(result_name, type_name):
+
+    action_weights = {
+        "success" : {"pass": 1, "shot": 5},
+        "fail" : {"pass": -1, "shot": 1}
+    }
+    try:
+      weight = action_weights[result_name][type_name]
+    except:
+      weight = 0
+    return weight
+
+def calc_game_momentum(actions, game_id, perspective_team_id = 0, weight_span = 3):
+
+    game_data = actions[actions['game_id'] == game_id]
+
+    # Convert time from seconds to minutes for easier processing
+    game_data['time_minutes'] = game_data['time_seconds'] // 60 + 45 * (game_data['period_id'] -1)
+
+    # Define action weights
+    action_weights = {"pass": 1, "shot": 2}
+
+    # Filter for relevant actions
+    relevant_actions = game_data[game_data['type_name'].isin(action_weights.keys())]
+
+    # Avoid SettingWithCopyWarning by creating a new DataFrame instead of modifying a slice
+    relevant_actions_fixed = relevant_actions.copy()
+    relevant_actions_fixed['action_weight'] = relevant_actions_fixed.apply(lambda x: calc_action_weight(x.result_name, x.type_name), axis=1)
+
+    # Group the data by game, minute, and team to count weighted actions and calculate the average x-coordinate
+    weighted_grouped_data = relevant_actions_fixed.groupby(['game_id', 'time_minutes', 'team_id']).agg(
+        weighted_actions=pd.NamedAgg(column='action_weight', aggfunc='sum'),
+        avg_start_x=pd.NamedAgg(column='start_x', aggfunc='mean')
+    ).reset_index()
+
+    # # Calculate momentum
+    weighted_grouped_data['momentum'] = ((weighted_grouped_data['avg_start_x'] - 50) / 50) * weighted_grouped_data['weighted_actions']
+
+    # # Dynamically determine the teams based on the data
+    teams = weighted_grouped_data['team_id'].unique()
+    if len(teams) != 2:
+        print("Error: There are not exactly two teams in the game data.")
+        return None
+
+    if perspective_team_id == 0:
+        team_1_id, team_2_id = teams[0], teams[1]
+    else:
+        team_1_id = perspective_team_id
+        team_2_id = np.setdiff1d(weighted_grouped_data['team_id'].unique(), perspective_team_id)[0]
+
+    # weighted average by teamId
+    team1_df = weighted_grouped_data[weighted_grouped_data['team_id'] == team_1_id]
+    team2_df = weighted_grouped_data[weighted_grouped_data['team_id'] == team_2_id]
+    team1_df['weighted_avg_momentum'] = team1_df.iloc[:,5].ewm(span=weight_span).mean()
+    team2_df['weighted_avg_momentum'] = -1 * team2_df.iloc[:,5].ewm(span=weight_span).mean()
+    team2_df['momentum'] = -1 * team2_df['momentum']
+
+    # # Adjust momentum calculation considering the team identity.
+    weighted_grouped_data = pd.concat([team1_df, team2_df])
+
+    # # Create a DataFrame for momentum difference per minute
+    momentum_per_minute = weighted_grouped_data.groupby('time_minutes')[['momentum', 'weighted_avg_momentum']].sum().reset_index()
+
+    # # Normalize the momentum values to be between -1 and 1
+    max_momentum = momentum_per_minute['momentum'].abs().max()
+    momentum_per_minute['momentum'] = momentum_per_minute['momentum'].apply(lambda x: x / max_momentum)
+    momentum_per_minute['weighted_avg_momentum'] = momentum_per_minute['weighted_avg_momentum'].apply(lambda x: x / max_momentum)
+
+    return momentum_per_minute
+
+def create_momentum_chart(game_momentum_df):
+    game_momentum_df['pos_momentum'] = game_momentum_df['momentum'].apply(lambda x: max(x, 0))
+    game_momentum_df['neg_momentum'] = game_momentum_df['momentum'].apply(lambda x: min(x, 0))
+
+    posChart = alt.Chart(game_momentum_df).mark_area().encode(
+        x="time_minutes",
+        y=alt.Y("pos_momentum", scale=alt.Scale(domain=[-1, 1]))
+    )
+
+    negChart = alt.Chart(game_momentum_df).mark_area().encode(
+        x="time_minutes",
+        y=alt.Y("neg_momentum", scale=alt.Scale(domain=[-1, 1])),
+        fill = alt.value("red")
+    )
+
+    game_momentum_df['pos_momentum_weighted'] = game_momentum_df['weighted_avg_momentum'].apply(lambda x: max(x, 0))
+    game_momentum_df['neg_momentum_weighted'] = game_momentum_df['weighted_avg_momentum'].apply(lambda x: min(x, 0))
+
+    posChart_w = alt.Chart(game_momentum_df).mark_area().encode(
+        x="time_minutes",
+        y=alt.Y("pos_momentum_weighted", scale=alt.Scale(domain=[-1, 1]))
+    )
+
+    negChart_w = alt.Chart(game_momentum_df).mark_area().encode(
+        x="time_minutes",
+        y=alt.Y("neg_momentum_weighted", scale=alt.Scale(domain=[-1, 1])),
+        fill = alt.value("red")
+    )
+
+    return posChart + negChart | posChart_w + negChart_w
+
+def get_games_by_team_id(actions, teamId):
+  return actions[actions['team_id'] == teamId]['game_id'].unique()
+
+def calc_team_season_momentum(actions, teamId):
+    game_ids = get_games_by_team_id(actions, teamId)
+
+    game_momentums = calc_game_momentum(actions, game_ids[0], teamId, 3)
+    for g in game_ids[1:]:
+        this_game_momentum = calc_game_momentum(actions, g, teamId, 3)
+        game_momentums = pd.concat([game_momentums, this_game_momentum])
+
+    return game_momentums.groupby('time_minutes').agg(
+        momentum=pd.NamedAgg(column='momentum', aggfunc='mean'),
+        weighted_avg_momentum=pd.NamedAgg(column='weighted_avg_momentum', aggfunc='mean')
+    ).reset_index()  
+  
+# Set up a dataframe with precalculated metrics per team
+def pass_success_rate(teamId, actionsGrouped):
+    return actionsGrouped[(actionsGrouped["team_id"] == teamId) & (actionsGrouped["type_name"] == "pass") & (actionsGrouped["result_name"] == "success")]["count"].sum() / actionsGrouped[(actionsGrouped["team_id"] == teamId) & (actionsGrouped["type_name"] == "pass")]["count"].sum()
+
+def crosses_per_shot(teamId, actionsGrouped):
+      return actionsGrouped[(actionsGrouped["team_id"] == teamId) & (actionsGrouped["type_name"] == "cross")]["count"].sum() / actionsGrouped[(actionsGrouped["team_id"] == teamId) & (actionsGrouped["type_name"] == "shot")]["count"].sum()
+
+def passes_per_shot(teamId, actionsGrouped):
+      return actionsGrouped[(actionsGrouped["team_id"] == teamId) & (actionsGrouped["type_name"] == "pass")]["count"].sum() / actionsGrouped[(actionsGrouped["team_id"] == teamId) & (actionsGrouped["type_name"] == "shot")]["count"].sum()
+
+def set_team_metrics_df(actions, teams, team_metrics):
+
+    actions_grouped = actions.groupby(['team_id', 'type_name', 'result_name']).size().reset_index(name="count")
+
+    return pd.DataFrame(
+        [[
+            t,
+            pass_success_rate(t, actions_grouped),
+            crosses_per_shot(t, actions_grouped),
+            passes_per_shot(t, actions_grouped)
+        ] for t in actions_grouped["team_id"].unique()],
+        columns = ["team_id"] + team_metrics
+    ).join(teams, lsuffix="team_id", rsuffix="wyId")
+
+def make_team_comparison_bar_chart(team_metrics_df, metric, multi, height=200):
+    base_bar = alt.Chart(team_metrics_df, title=metric).mark_bar().encode(
+      x="name:N",
+      color= "name:N",
+      y= alt.Y(metric, axis=alt.Axis(title=None)),
+    ).transform_filter(multi).properties(height=height)
+    text= base_bar.mark_text(angle = 270, align="center", yOffset=50, fontWeight="bold").encode(text="name:N", color=alt.ColorValue("black"))
+    return (base_bar + text)
+
+def create_team_comparison_charts(team_metrics_df, team_metrics):
+    sphere = alt.sphere()
+    graticule = alt.graticule(step=[10, 10])
+    # lats = alt.sequence(start=-30, stop=71, step=10, as_='lats')
+    # lons = alt.sequence(start=-90, stop=91, step=10, as_='lons')
+
+    width = 800
+    height = 600
+
+    # Source of land data
+    source = alt.topo_feature(data.world_110m.url, 'countries')
+
+    # Layering and configuring the components
+    base = alt.layer(
+        alt.Chart(sphere).mark_geoshape(fill='none'),
+        alt.Chart(graticule).mark_geoshape(stroke='gray', strokeWidth=0.5),
+        alt.Chart(source).mark_geoshape(fill='lightgray', stroke='gray')
+    ).properties(width=width, height=height)
+
+    projections = {
+        "Albers": {
+            "type": "albers",
+            "center": [-10, 50],
+            "rotate": [-20, 0],
+            "translate": [width/2, height/2],
+            "scale": 1100,
+            "precision": 0.1
+        },
+    }
+    geo_chart = base.properties(projection=projections['Albers'])
+
+    multi = alt.selection_multi(on='click', nearest=False, empty = 'none', bind='legend')
+    geo_points = alt.Chart(team_metrics_df).mark_circle().encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        size=alt.condition(multi, alt.value(60),alt.value(40)),
+        shape=alt.condition(multi, alt.ShapeValue("diamond"), alt.ShapeValue("circle")),
+        tooltip='officialName',
+        color= alt.condition(multi, "name:N",alt.ColorValue('black'))
+        # color = alt.Color("name:N", legend=None) I can't figure out how to suppress the legend while keeping the condition?
+    ).add_selection(
+        multi
+    )
+
+    barChartProperties = {'height': 200}
+
+    barChart1 = make_team_comparison_bar_chart(team_metrics_df, team_metrics[0], multi)
+    barChart2 = make_team_comparison_bar_chart(team_metrics_df, team_metrics[1], multi)
+    barChart3 = make_team_comparison_bar_chart(team_metrics_df, team_metrics[2], multi, height=201)
+
+    return (geo_chart + geo_points) & (barChart1 | barChart2 | barChart3)
+
+def main3():
+    actions = load_data('actions.csv')
+    teams = load_data('teams.csv')
+    
+    pd.options.mode.chained_assignment = None
+
+    # Get lat/longs for each city
+    teams[['latitude', 'longitude']] = teams.apply(lambda x: get_lat_long(x.city), axis=1, result_type='expand')
+
+    # Sidebar - Team selection
+    st.sidebar.header('Team Selection')
+    sorted_unique_teams = sorted(teams['team_id'].unique())
+    selected_team = st.sidebar.selectbox('Choose a team', sorted_unique_teams)
+
+    st.header('Team Momentum')
+    create_momentum_chart(calc_team_season_momentum(actions, selected_team))
+
+    st.header('Team Comparisons')
+    team_metrics = ["Pass Success Rate", "Crosses / Shot", "Passes / Shot"]
+    team_metrics_df = set_team_metrics_df(actions, teams, team_metrics)
+
+    create_team_comparison_charts(team_metrics_df, team_metrics)
+
 def main():
     st.title('KickLogic - Soccer Analytics')
     app_choice = st.selectbox('Choose your analysis type:', ['Player Role', 'Match', 'Player Valuation', 'Club'])
@@ -322,6 +558,8 @@ def main():
         main2()
     elif app_choice == 'Match':
         main1()
+    elif app_choice == 'Club':
+        main3()
 
 if __name__ == '__main__':
     main()
