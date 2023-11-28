f05993dc4745d2e616506cda105b7d1a2de0a069
Abel Ninan <abel.j.ninan@gmail.com>
2023-11-28 14:05:10 -0500

    Update Soccer_Streamlit.py

--- Soccer_Streamlit.py
+++ Soccer_Streamlit.py
@@ -485,6 +485,9 @@ def main2():
 
     role_stats = load_data('streamlit_stats.csv')
     st.dataframe(role_stats)
+
+    clean_positions  = sorted(role_stats['clean_positions'].unique())
+    position_choice = st.selectbox('Choose a Player Position:', clean_positions)
         
 def calc_action_weight(result_name, type_name):
 
