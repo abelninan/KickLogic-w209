2185bf517c87ddc7c0e99a389a1503c93b1b9032
Abel Ninan <abel.j.ninan@gmail.com>
2023-12-02 22:04:21 -0500

    deleting lines of code

--- Soccer_Streamlit.py
+++ Soccer_Streamlit.py
@@ -6,11 +6,6 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
-
-# from geopy.geocoders import Nominatim
-# from ctypes import alignment
-# from vega_datasets import data
-
 # Function to load data
 @st.cache_data  # This function will be cached
 def load_data(path):
