fcc32fb91a322ecc1dafa9ff09539c204d748bac
dan-nealon <101353912+dan-nealon@users.noreply.github.com>
2023-12-08 01:53:50 -0500

    Update Soccer_Streamlit.py

--- Soccer_Streamlit.py
+++ Soccer_Streamlit.py
@@ -137,7 +137,7 @@ def create_passing_map(game_data, selected_team):
     # Determine if the pass was successful
     pass_actions['pass_outcome'] = pass_actions['result_name'].apply(lambda x: 'success' if x == 'success' else 'fail')
     pass_actions['angle'] = pass_actions.apply(calculate_angle, axis=1)
-
+    st.write(pass_actions.head(50))
     # Define field dimensions; you might adjust these based on the coordinate system in your data
     # Store as variables we can easily reuse for the plots
     field_length_min =  0.0
