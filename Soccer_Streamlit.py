f744f2c3cac1542e5126602837f6bbe35b214fb9
dan-nealon <101353912+dan-nealon@users.noreply.github.com>
2023-12-08 00:37:37 -0500

    Update Soccer_Streamlit.py

--- Soccer_Streamlit.py
+++ Soccer_Streamlit.py
@@ -397,7 +397,7 @@ def display_game_statistics(game_data):
     # Create labels using mark_text
     labels = base.mark_text(
         #align=alt.condition(alt.datum['Team'] == team_names[0], alt.value('right'), alt.value('left')),
-        align = 'center',
+        #align = 'center',
         baseline='middle',  # Center the text vertically within the bars
         #dx=alt.condition(alt.datum['Team'] == team_names[0], alt.value(5), alt.value(-5)),
         #dx = 0,
