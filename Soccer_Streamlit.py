ec07fe2ca6d404358824d6b1a42802b26364de8f
dan-nealon <101353912+dan-nealon@users.noreply.github.com>
2023-12-08 00:22:39 -0500

    Update Soccer_Streamlit.py

--- Soccer_Streamlit.py
+++ Soccer_Streamlit.py
@@ -396,13 +396,14 @@ def display_game_statistics(game_data):
      
     # Create labels using mark_text
     labels = base.mark_text(
-        align='center',
-        baseline='left',
+        align='left',
+        baseline='middle',
         dx=0,  # Adjust this value to control the label position
         dy=0,  # Adjust this value to control the label position
     ).encode(
         text='Count:Q',
-        color=alt.value('white')  # Set the text color to white
+        color=alt.value('white'),  # Set the text color to white
+        x='sum(Percentage):Q
     )
     
     # Layer the bar chart with text
