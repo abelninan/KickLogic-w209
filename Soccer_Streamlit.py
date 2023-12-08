44444a0597b18ded2a36894e4e652a34b0fd57eb
dan-nealon <101353912+dan-nealon@users.noreply.github.com>
2023-12-08 01:00:58 -0500

    Update Soccer_Streamlit.py

--- Soccer_Streamlit.py
+++ Soccer_Streamlit.py
@@ -412,7 +412,6 @@ def display_game_statistics(game_data):
     labels_team1 = base.transform_filter(alt.datum['Team'] == teams[0]).mark_text(
         align='left',
         baseline='middle',
-        dx=5,
     ).encode(
         text=alt.Text('Count:Q', format=','),
         color=alt.value('white'),
@@ -422,7 +421,6 @@ def display_game_statistics(game_data):
     labels_team2 = base.transform_filter(alt.datum['Team'] == teams[1]).mark_text(
         align='right',
         baseline='middle',
-        dx=-5,
     ).encode(
         text=alt.Text('Count:Q', format=','),
         color=alt.value('white'),
